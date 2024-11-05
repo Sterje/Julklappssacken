6776720a2a7959ecf53a1124ab4f7314f3930c80
Jesper Eriksson Stenberg <jespermail@gmail.com>
2024-11-05 22:06:43 +0100

    Learning arrays
    Learning arrays, work in progress

---
+++ script.js
@@ -0,0 +1,24 @@
+// Skapar själva julklappssäcken, en array som kallas currentBag
+const currentBag = [];
+
+// Funktion för att lägga till paket i säcken.
+function addItem() {
+
+        // Hämtar värdet från textfältet "giftitem". VIktigt att lägga till .value
+        const giftInput = document.getElementById("giftItem").value;
+            // Lägger till värdet från giftInput till array currentBag
+            currentBag.push(giftInput);
+}
+
+// Här är funktionen för att visa innehållet i säcken.
+function showBag() {
+
+//För att skapa en oordnad lista att skriva ut innehållet i säcken i. Loopas igenom tills allt är utskrivet.
+let text = "<ul>";
+for (let i = 0; i < currentBag.length; i++) {
+    document.getElementById("demo").innerHTML = text += "<li>" + currentBag[i] + "</li>";
+}
+text += "</ul>";
+}
+
+    
