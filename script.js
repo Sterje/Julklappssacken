ee4b7d55ccdfbe3f9b1fc8dc9adfdffcc1856579
Jesper Eriksson Stenberg <jespermail@gmail.com>
2024-11-06 08:47:07 +0100

    Update

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function addItem() {
         const giftInput = document.getElementById("giftItem").value;
             // Lägger till värdet från giftInput till array currentBag
             currentBag.push(giftInput);
+            // Rensar input fältet när objekt lagts till i säcken.
+            giftItem.value = "";
 }
 
 // Här är funktionen för att visa innehållet i säcken.
@@ -16,7 +18,7 @@ function showBag() {
 //För att skapa en oordnad lista att skriva ut innehållet i säcken i. Loopas igenom tills allt är utskrivet.
 let text = "<ul>";
 for (let i = 0; i < currentBag.length; i++) {
-    document.getElementById("demo").innerHTML = text += "<li>" + currentBag[i] + "</li>";
+    document.getElementById("listItems").innerHTML = text += "<li>" + currentBag[i] + "</li>";
 }
 text += "</ul>";
 }
